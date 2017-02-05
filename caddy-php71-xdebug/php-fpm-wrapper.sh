e71665ddaefc226295ec3d66a88e24d9fca60cbd
zhoujin7 <zhoujin7@foxmail.com>
2017-02-05 15:15:55 +0800

    nothing

---
+++ caddy-php71-xdebug/php-fpm-wrapper.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [ ! -z "$PHP_BEFORE" ]; then
+    eval $PHP_BEFORE
+fi
+
+if [ -f /srv/composer.lock ]; then
+    cd /srv
+    composer install ${PHP_COMPOSER_FLAGS}
+fi
+
+/usr/local/sbin/php-fpm -c /usr/local/etc/php/ --fpm-config /usr/local/etc/php-fpm.conf --daemonize --force-stderr
+
+if [ ! -z "$PHP_AFTER" ]; then
+    eval $PHP_AFTER
+fi
